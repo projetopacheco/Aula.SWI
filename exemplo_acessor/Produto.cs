68e3981a0c007ec9546893d56a0a428dc9222710
projetopacheco <100379704+projetopacheco@users.noreply.github.com>
2023-03-29 16:22:32 -0300

    Métodos Acessores

---
+++ exemplo_acessor/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace exemplo_acessor;
+class Produto
+{
+    private string? Nome {get;set;}
+    private double Preco {get;set;}
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("Preco: " + this.Preco);
+    }
+    public void AlteraPreco(double preco){
+        if(preco >=0){
+            this.Preco = preco;
+        }else{
+            Console.WriteLine("VALOR INVÁLIDO!");
+        }
+    }
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+}
---
+++ exemplo_acessor/Program.cs
@@ -0,0 +1,10 @@
+namespace exemplo_acessor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto();
+
+        p1.MostraDados();
+    }
+    }
