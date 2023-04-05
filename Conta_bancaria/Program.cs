5527b6721c4de8fc5f9700caecc9ed42a7f0b183
projetopacheco <100379704+projetopacheco@users.noreply.github.com>
2023-04-05 16:15:24 -0300

    Add files via upload

---
+++ Conta_bancaria/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Conta_bancaria;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercicio Conta bancária Etec MCM");
+        Conta conta = new Conta();
+
+        conta.nomeCliente = "Anderson Vanin";
+        
+        conta.limite = 1800;
+
+        conta.depositar(200);
+
+        conta.sacar(50);
+
+        double saldo = conta.ConsultaSaldo();
+        Console.WriteLine("Seu saldo é de: " + saldo);
+    }
+}
