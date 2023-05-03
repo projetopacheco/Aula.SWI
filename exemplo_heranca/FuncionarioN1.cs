889238fdc5fe83ae15c96c76bbe46b9a8046b5fe
projetopacheco <100379704+projetopacheco@users.noreply.github.com>
2023-05-03 16:40:00 -0300

    heranca_polimorfismo

---
+++ exemplo_heranca/FuncionarioN1.cs
@@ -0,0 +1,7 @@
+namespace exemplo_heranca;
+class FuncionarioN1:Funcionario
+{
+  public override double Lucro(){
+    return base.Lucro()*0.01;
+  }
+}
---
+++ exemplo_heranca/FuncionarioN3.cs
@@ -0,0 +1,8 @@
+namespace exemplo_heranca;
+class FuncionarioN3:Funcionario
+{
+
+  public override double Lucro(){
+   return base.Lucro() *0.03;
+  }
+}
